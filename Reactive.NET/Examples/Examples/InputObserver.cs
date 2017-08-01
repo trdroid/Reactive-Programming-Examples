fda64a3e14f681e86770d50478079ed05f77e7f0
Jai <gruprog@gmail.com>
2017-08-01 14:23:49 -0700

    an IObserver example

---
+++ Reactive.NET/Examples/Examples/InputObserver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples
+{
+    public class InputObserver : IObserver<string>
+    {
+        private bool completed = false;
+
+        public void OnCompleted()
+        {
+            if (completed)
+            {
+                this.OnError(new Exception("The observer has already been completed"));                
+            }
+            else
+            {
+                completed = true;
+                Console.WriteLine("In onCompleted");
+            }
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("In onError; {0}", error.Message);
+        }
+
+        public void OnNext(string input)
+        {
+            if (completed)
+            {
+                this.OnError(new Exception("The observer has already been completed"));                
+            }
+            else
+            {
+                Console.WriteLine("The observer received the input: {0}", input);
+            }            
+        }
+    }
+}
---
+++ Reactive.NET/Examples/Examples/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputObserver inputObserver = new InputObserver();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.Equals("BYE"))
+                {
+                    System.Environment.Exit(0);
+                }
+                else if (input.Equals("END"))
+                {
+                    inputObserver.OnCompleted();
+
+                    Console.WriteLine("The observer has been sent an onCompleted message ...");
+                    Console.WriteLine("The observer cannot be sent any more messages ...");
+                }
+                else
+                {
+                    inputObserver.OnNext(input);
+                }
+            }
+        }
+    }
+}
