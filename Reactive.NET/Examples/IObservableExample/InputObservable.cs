4a459bc03ed713054ce640b7015c2296bb4d2566
Jai <gruprog@gmail.com>
2017-08-02 09:41:50 -0700

    an IObservable example

---
+++ Reactive.NET/Examples/IObservableExample/InputObservable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace IObservableExample
+{
+    // An observable is a producer that produces data that any observers can subscribe to and consume.
+    // This makes observers consumers of that data. 
+    public class InputObservable : IObservable<string>, IDisposable
+    {
+        private readonly List<IObserver<string>> subscribers = new List<IObserver<string>>();
+
+        private readonly CancellationTokenSource cancellationSource;
+        private readonly CancellationToken cacellationToken;
+
+        public InputObservable()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable Subscribe(IObserver<string> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
